dfac03f524241589600348675d8b927af8972746
nandini <happynandini.006@gmail.com>
2024-10-24 16:52:57 +0530

    Initial commit5

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('cypress');
 const sendFailureEmail = require('./send-email'); // Import your send-email.js script
+require('dotenv').config();
 
 module.exports = defineConfig({
+  env: {
+    EMAIL_USER: process.env.EMAIL_USER, // Access from .env
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD, // Access from .env
+  },
   e2e: {
     setupNodeEvents(on, config) {
       on('task', {
